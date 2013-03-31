1217bece4f4c4e1ec0906c7170bbdcdf268ab886
Eugene Lazutkin <eugene@lazutkin.com>
2013-03-30 20:10:15 -0500

    Renamed assert() to test().

--- tests/tests.js
+++ tests/tests.js
@@ -36,7 +36,7 @@
 		}
 	}
 
-	function assert(condition){
+	function test(condition){
 		return "submit('" + quoteString(condition) + "', (" + condition + "))";
 	}
 
@@ -53,114 +53,114 @@
 
 	var tests = [
 		function test_constants(){
-			eval(assert("unify(1, 1)"));
-			eval(assert("unify(0, 0)"));
-			eval(assert("unify(null, null)"));
-			eval(assert("unify(undefined, undefined)"));
-			eval(assert("unify(true, true)"));
-			eval(assert("unify(false, false)"));
-			eval(assert("unify('', '')"));
-			eval(assert("unify('1', '1')"));
-			eval(assert("unify(Infinity, Infinity)"));
-			eval(assert("unify(-Infinity, -Infinity)"));
-			eval(assert("unify(NaN, NaN)"));
-			eval(assert("!unify(1, 2)"));
-			eval(assert("!unify(1, true)"));
-			eval(assert("!unify(1, '1')"));
-			eval(assert("!unify(1, [])"));
-			eval(assert("!unify(1, {})"));
+			eval(test("unify(1, 1)"));
+			eval(test("unify(0, 0)"));
+			eval(test("unify(null, null)"));
+			eval(test("unify(undefined, undefined)"));
+			eval(test("unify(true, true)"));
+			eval(test("unify(false, false)"));
+			eval(test("unify('', '')"));
+			eval(test("unify('1', '1')"));
+			eval(test("unify(Infinity, Infinity)"));
+			eval(test("unify(-Infinity, -Infinity)"));
+			eval(test("unify(NaN, NaN)"));
+			eval(test("!unify(1, 2)"));
+			eval(test("!unify(1, true)"));
+			eval(test("!unify(1, '1')"));
+			eval(test("!unify(1, [])"));
+			eval(test("!unify(1, {})"));
 		},
 		function test_anyvar(){
-			eval(assert("unify(_, 1)"));
-			eval(assert("unify(_, 2)"));
-			eval(assert("unify(_, true)"));
-			eval(assert("unify(_, '1')"));
-			eval(assert("unify(_, [])"));
-			eval(assert("unify(_, {})"));
-			eval(assert("unify(1, _)"));
-			eval(assert("unify(2, _)"));
-			eval(assert("unify(true, _)"));
-			eval(assert("unify('1', _)"));
-			eval(assert("unify([], _)"));
-			eval(assert("unify({}, _)"));
+			eval(test("unify(_, 1)"));
+			eval(test("unify(_, 2)"));
+			eval(test("unify(_, true)"));
+			eval(test("unify(_, '1')"));
+			eval(test("unify(_, [])"));
+			eval(test("unify(_, {})"));
+			eval(test("unify(1, _)"));
+			eval(test("unify(2, _)"));
+			eval(test("unify(true, _)"));
+			eval(test("unify('1', _)"));
+			eval(test("unify([], _)"));
+			eval(test("unify({}, _)"));
 		},
 		function test_exact_arrays(){
-			eval(assert("unify([], [])"));
-			eval(assert("unify([1], [1])"));
-			eval(assert("unify([1,2], [1,2])"));
-			eval(assert("!unify([], [1])"));
-			eval(assert("!unify([1], [2])"));
-			eval(assert("!unify([2,1], [1,2])"));
-			eval(assert("unify([1,_,3], [_,2,_])"));
-			eval(assert("unify([_,_,3], [1,_,_])"));
-			eval(assert("unify([[]], [[]])"));
-			eval(assert("unify([[], []], [[], []])"));
+			eval(test("unify([], [])"));
+			eval(test("unify([1], [1])"));
+			eval(test("unify([1,2], [1,2])"));
+			eval(test("!unify([], [1])"));
+			eval(test("!unify([1], [2])"));
+			eval(test("!unify([2,1], [1,2])"));
+			eval(test("unify([1,_,3], [_,2,_])"));
+			eval(test("unify([_,_,3], [1,_,_])"));
+			eval(test("unify([[]], [[]])"));
+			eval(test("unify([[], []], [[], []])"));
 		},
 		function test_exact_objects(){
-			eval(assert("unify({}, {})"));
-			eval(assert("unify({a: 1}, {a: 1})"));
-			eval(assert("unify({a: 1, b: 2}, {b: 2, a: 1})"));
-			eval(assert("!unify({}, {a: 1})"));
-			eval(assert("!unify({a: 1}, {a: 2})"));
-			eval(assert("!unify({a: 1}, {b: 1})"));
-			eval(assert("unify({a: _, b: 2}, {a: 1, b: _})"));
-			eval(assert("unify({a: {a: 1}, b: 2}, {a: {a: 1}, b: 2})"));
-			eval(assert("!unify({a: {a: 1}, b: 2}, {a: {a: 3}, b: 2})"));
-			eval(assert("!unify({a: {a: 1}, b: 2}, {a: {a: 1}, b: 3})"));
+			eval(test("unify({}, {})"));
+			eval(test("unify({a: 1}, {a: 1})"));
+			eval(test("unify({a: 1, b: 2}, {b: 2, a: 1})"));
+			eval(test("!unify({}, {a: 1})"));
+			eval(test("!unify({a: 1}, {a: 2})"));
+			eval(test("!unify({a: 1}, {b: 1})"));
+			eval(test("unify({a: _, b: 2}, {a: 1, b: _})"));
+			eval(test("unify({a: {a: 1}, b: 2}, {a: {a: 1}, b: 2})"));
+			eval(test("!unify({a: {a: 1}, b: 2}, {a: {a: 3}, b: 2})"));
+			eval(test("!unify({a: {a: 1}, b: 2}, {a: {a: 1}, b: 3})"));
 		},
 		function test_variables(){
 			var result = unify(1, 1);
-			eval(assert("result && unify(result.values, {})"));
+			eval(test("result && unify(result.values, {})"));
 			result = unify(1, _);
-			eval(assert("result && unify(result.values, {})"));
+			eval(test("result && unify(result.values, {})"));
 			result = unify(1, v("x"));
-			eval(assert("result && unify(result.values, {x: 1})"));
+			eval(test("result && unify(result.values, {x: 1})"));
 			result = unify(v("y"), v("x"));
-			eval(assert("result && unify(result.values, {})"));
-			eval(assert("result && unify(result.variables, {x: {x: 1, y: 1}, y: {x: 1, y: 1}})"));
+			eval(test("result && unify(result.values, {})"));
+			eval(test("result && unify(result.variables, {x: {x: 1, y: 1}, y: {x: 1, y: 1}})"));
 			result = unify(v("y"), _);
-			eval(assert("result && unify(result.values, {})"));
+			eval(test("result && unify(result.values, {})"));
 			result = unify([1, v("x")], [v("y"), 2]);
-			eval(assert("result && unify(result.values, {x: 2, y: 1})"));
+			eval(test("result && unify(result.values, {x: 2, y: 1})"));
 			result = unify({a: 1, b: v("x")}, {a: v("y"), b: 2});
-			eval(assert("result && unify(result.values, {x: 2, y: 1})"));
+			eval(test("result && unify(result.values, {x: 2, y: 1})"));
 			result = unify({a: 1, b: v("x")}, {a: v("y"), c: 2});
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify({c: 1, b: v("x")}, {a: v("y"), b: 2});
-			eval(assert("!result"));
+			eval(test("!result"));
 		},
 		function test_regexes(){
-			eval(assert("unify(/\\b\\w+\\b/, /\\b\\w+\\b/)"));
-			eval(assert("!unify(/\\b\\w+\\b/m, /\\b\\w+\\b/)"));
-			eval(assert("!unify(/\\b\\w+\\b/m, /\\b\\w+\\b/g)"));
-			eval(assert("!unify(/\\b\\w+\\b/, /\\b\\w+\\b/i)"));
-			eval(assert("!unify(/\\b\\w+\\b/, 1)"));
-			eval(assert("unify(/\\b\\w+\\b/, new RegExp('\\\\b\\\\w+\\\\b'))"));
+			eval(test("unify(/\\b\\w+\\b/, /\\b\\w+\\b/)"));
+			eval(test("!unify(/\\b\\w+\\b/m, /\\b\\w+\\b/)"));
+			eval(test("!unify(/\\b\\w+\\b/m, /\\b\\w+\\b/g)"));
+			eval(test("!unify(/\\b\\w+\\b/, /\\b\\w+\\b/i)"));
+			eval(test("!unify(/\\b\\w+\\b/, 1)"));
+			eval(test("unify(/\\b\\w+\\b/, new RegExp('\\\\b\\\\w+\\\\b'))"));
 		},
 		function test_dates(){
-			eval(assert("unify(new Date(2013, 6, 4), new Date(2013, 6, 4))"));
-			eval(assert("!unify(new Date(2013, 6, 4), new Date(2012, 6, 4))"));
-			eval(assert("!unify(new Date(2013, 6, 4), new Date(2013, 6, 4, 6))"));
-			eval(assert("unify(new Date(2013, 6, 4, 6), new Date(2013, 6, 4, 6))"));
+			eval(test("unify(new Date(2013, 6, 4), new Date(2013, 6, 4))"));
+			eval(test("!unify(new Date(2013, 6, 4), new Date(2012, 6, 4))"));
+			eval(test("!unify(new Date(2013, 6, 4), new Date(2013, 6, 4, 6))"));
+			eval(test("unify(new Date(2013, 6, 4, 6), new Date(2013, 6, 4, 6))"));
 		},
 		function test_open_structures(){
-			eval(assert("unify({a: 1, b: 2, c: 3}, open({a: 1}))"));
-			eval(assert("unify(open({a: 1}), {a: 1, b: 2, c: 3})"));
-			eval(assert("unify([1, 2, 3], open([1,2]))"));
-			eval(assert("unify(open([1, 2]), [1, 2, 3])"));
-			eval(assert("unify(open({a: 1}), open({b: 2}))"));
-			eval(assert("unify(open([1]), open([1, 2]))"));
+			eval(test("unify({a: 1, b: 2, c: 3}, open({a: 1}))"));
+			eval(test("unify(open({a: 1}), {a: 1, b: 2, c: 3})"));
+			eval(test("unify([1, 2, 3], open([1,2]))"));
+			eval(test("unify(open([1, 2]), [1, 2, 3])"));
+			eval(test("unify(open({a: 1}), open({b: 2}))"));
+			eval(test("unify(open([1]), open([1, 2]))"));
 		},
 		function test_soft_structures(){
 			var x = v("x");
 			var result = unify([soft({a: 1}), soft({b: 2})], soft([x, x]));
-			eval(assert("result && isSoft(x.get(result))"));
-			eval(assert("result && x.get(result).type === 'soft'"));
-			eval(assert("result && unify(x.get(result).object, {a: 1, b: 2})"));
+			eval(test("result && isSoft(x.get(result))"));
+			eval(test("result && x.get(result).type === 'soft'"));
+			eval(test("result && unify(x.get(result).object, {a: 1, b: 2})"));
 			result = unify([soft({a: 1}), x], soft([x, soft({b: 2})]));
-			eval(assert("result && isSoft(x.get(result))"));
-			eval(assert("result && x.get(result).type === 'soft'"));
-			eval(assert("result && unify(x.get(result).object, {a: 1, b: 2})"));
+			eval(test("result && isSoft(x.get(result))"));
+			eval(test("result && x.get(result).type === 'soft'"));
+			eval(test("result && unify(x.get(result).object, {a: 1, b: 2})"));
 		},
 		function test_complex_structures(){
 			var x = v("x"), y = v("y");
@@ -188,17 +188,17 @@
 				left: open({left: y}),
 				right: open({right: y})
 			});
-			eval(assert("result && x.get(result) === 0"));
-			eval(assert("result && unify(y.get(result), {value: 3})"));
+			eval(test("result && x.get(result) === 0"));
+			eval(test("result && unify(y.get(result), {value: 3})"));
 		},
 		function test_soft_presets(){
 			var x = v("x"), env = unify(x, soft({}));
 			var result = unify([1], [x], env);
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify([open({a: 1}), open({b: 2})], [x, x], env);
-			eval(assert("result && isSoft(x.get(result))"));
-			eval(assert("result && x.get(result).type === 'soft'"));
-			eval(assert("result && unify(x.get(env).object, {a: 1, b: 2})"));
+			eval(test("result && isSoft(x.get(result))"));
+			eval(test("result && x.get(result).type === 'soft'"));
+			eval(test("result && unify(x.get(env).object, {a: 1, b: 2})"));
 		},
 		function test_preprocess(){
 			var l = {
@@ -217,19 +217,19 @@
 					z: "ah!"
 				};
 			var result = unify(l, r);
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify(l, preprocess(r));
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify(l, preprocess(r, true));
-			eval(assert("result"));
+			eval(test("result"));
 			result = unify(l.y, {c: [1, 2]});
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify(l.y, preprocess({c: [1, 2]}));
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify(l.y, preprocess({c: [1, 2]}, false, true));
-			eval(assert("!result"));
+			eval(test("!result"));
 			result = unify(l.y, preprocess({c: [1, 2]}, true, true));
-			eval(assert("result"));
+			eval(test("result"));
 		}
 	];
 
